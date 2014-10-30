3ed85557e0e2a6817842cc5dfaa323baefca0deb
notoroid <noto@irimasu.com>
2014-10-30 21:56:32 +0900

    Remove commented source code.

--- Lib/IDPAWGroupView.m
+++ Lib/IDPAWGroupView.m
@@ -61,18 +61,6 @@
     return hitTest;
 }
 
-//- (void)drawGroupWithFrame: (CGRect)frame;
-//{
-//    //// Color Declarations
-//    UIColor* color = [UIColor colorWithRed: 0.262 green: 0.871 blue: 0.312 alpha: 1];
-//    
-//    //// Rectangle Drawing
-//    UIBezierPath* rectanglePath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + 0.5, CGRectGetMinY(frame) + 0.5, CGRectGetWidth(frame) - 1, CGRectGetHeight(frame) - 1)];
-//    [color setStroke];
-//    rectanglePath.lineWidth = 1;
-//    [rectanglePath stroke];
-//}
-
 - (void)drawRect:(CGRect)rect
 {
     _hittestBezier = nil;
